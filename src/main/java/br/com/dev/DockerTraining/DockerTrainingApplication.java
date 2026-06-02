723f597dc1d958bc65b29c21cfb22b9427246838
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-06-01 22:30:27 -0300

    Initial commit

---
+++ src/main/java/br/com/dev/DockerTraining/DockerTrainingApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.DockerTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/DockerTraining/DockerTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.DockerTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
